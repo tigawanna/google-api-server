08ee22752b2bcd314367fecd038bf5a972e1e057
tigawanna <denniskinuthiaw@gmail.com>
2022-06-11 10:14:51 +0300

    added calender functions

---
+++ src/calenderutils.ts
@@ -0,0 +1,77 @@
+const {google} = require('googleapis');
+
+
+const startTime=new Date();
+startTime.setDate(startTime.getDate() + 2)
+
+const endTime=new Date();
+startTime.setDate(startTime.getDate() + 4)
+
+
+
+export function listEvents(auth:any) {
+    const calendar = google.calendar({version: 'v3', auth});
+    calendar.events.list({
+      calendarId: 'primary',
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime',
+    }, (err:any, res:any) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const events = res.data.items;
+      if (events.length) {
+        console.log('Upcoming 10 events:');
+        events.map((event:any, i:any) => {
+          const start = event.start.dateTime || event.start.date;
+          console.log(`${start} - ${event.summary}`);
+        });
+      } else {
+        console.log('No upcoming events found.');
+      }
+    });
+  }
+
+
+export const addCalEvents=(auth:any)=>{
+const calendar = google.calendar({version: 'v3', auth});
+    var event = {
+        'summary': 'Google I/O 2015',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+          'dateTime': '2015-05-28T09:00:00-07:00',
+          'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+          'dateTime': '2015-05-28T17:00:00-07:00',
+          'timeZone': 'America/Los_Angeles',
+        },
+        'recurrence': [
+          'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+          {'email': 'lpage@example.com'},
+          {'email': 'sbrin@example.com'},
+        ],
+        'reminders': {
+          'useDefault': false,
+          'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10},
+          ],
+        },
+      };
+      
+      calendar.events.insert({
+        auth: auth,
+        calendarId: 'primary',
+        resource: event,
+      }, function(err:any, event:any) {
+        if (err) {
+          console.log('There was an error contacting the Calendar service: ' + err);
+          return;
+        }
+        console.log('Event created: %s', event.htmlLink);
+      });
+}
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { initChecks } from '../middleware/initialChecks';
+import { listEvents } from '../calenderutils';
 const nodemailer = require("nodemailer");
 const {google} = require('googleapis');
 
@@ -59,28 +60,7 @@ res.send(result)
 router.get('/calender',initChecks, (req: Request, res: Response) => {
 const auth2Client=req.query.oauthClient
 
-  function listEvents(auth:any) {
-    const calendar = google.calendar({version: 'v3', auth});
-    calendar.events.list({
-      calendarId: 'primary',
-      timeMin: (new Date()).toISOString(),
-      maxResults: 10,
-      singleEvents: true,
-      orderBy: 'startTime',
-    }, (err:any, res:any) => {
-      if (err) return console.log('The API returned an error: ' + err);
-      const events = res.data.items;
-      if (events.length) {
-        console.log('Upcoming 10 events:');
-        events.map((event:any, i:any) => {
-          const start = event.start.dateTime || event.start.date;
-          console.log(`${start} - ${event.summary}`);
-        });
-      } else {
-        console.log('No upcoming events found.');
-      }
-    });
-  }
+
       listEvents(auth2Client)
       res.send("calender route")
   });
